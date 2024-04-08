916476f85fd338a997ab3df6f77520c77200c545
WHITE DEVIL - SM <105021702+WhiteDevilSM@users.noreply.github.com>
2024-04-09 01:27:21 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
         return default
 
 class script(object):
-    HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://omegalinks.in/ref/sachinsachu3603')
+    HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://shrinkforearn.in/ref/101105455849308677653')
     START_TXT = environ.get("START_TXT", '''<b>Hello {} 👋🏻 Im OTT Search Bot I can share Movies and Series 😁.</b>
 
 <i>Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ sᴇᴇ ᴛʜᴇ ᴍᴀɢɪᴄ ᴏʀ ʀᴇᴀᴅ ᴍᴏʀᴇ ғʀᴏᴍ ᴛʜᴇ ᴍᴇɴᴜ ʙᴇʟᴏᴡ</i>''')
